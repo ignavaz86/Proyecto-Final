88c92230e4a9e7415b65281279d320cee89aa751
ignavaz86 <vazquezignacioa@gmail.com>
2023-01-22 18:57:29 -0300

    Se agrega botones ver y ocultar form

--- codigo.js
+++ codigo.js
@@ -8,7 +8,17 @@ document.getElementById('check-item').addEventListener('click', function enableD
     contenido_body.classList.toggle("dark-mode");
 })
 
+document.getElementById('mostrarForm').addEventListener('click', function(){
+    document.getElementById('ocultarForm').style.display = 'block';
+    document.getElementById('mostrarForm').style.display = 'none';
+    document.getElementById('formulario').style.display = 'block';
+})
 
+document.getElementById('ocultarForm').addEventListener('click', function(){
+    document.getElementById('mostrarForm').style.display = 'block';
+    document.getElementById('ocultarForm').style.display = 'none';
+    document.getElementById('formulario').style.display = 'none';
+})
 
 
 
