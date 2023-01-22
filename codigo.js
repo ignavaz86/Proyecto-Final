4557702753e94ba316019fb10d1f0594ecef09ea
ignavaz86 <vazquezignacioa@gmail.com>
2023-01-22 17:24:27 -0300

    Se agrega Proyecto CV

---
+++ codigo.js
@@ -0,0 +1,22 @@
+
+
+
+//Dark-mode
+
+document.getElementById('check-item').addEventListener('click', function enableDarkMode() {
+    let contenido_body = document.body;
+    contenido_body.classList.toggle("dark-mode");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
